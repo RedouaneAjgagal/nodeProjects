0d857d4a55a179d7283bd385c7be5794c52b64ec
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-08 19:05:43 +0100

    Display dynamic content on posts/postName and use lodash for kebabCase

--- blog/app.js
+++ blog/app.js
@@ -5,12 +5,13 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const _ = require('lodash')
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-let posts = []
+let posts = [{ title: 'Welcome Ota', content: 'Some demo text' }]
 
 app.get('/', (res, req) => {
   req.render('home', { content: homeStartingContent, posts });
@@ -27,15 +28,19 @@ app.get('/compose', (res, req) => {
 
 app.get('/posts/:postId', (req, res) => {
   const params = req.params.postId
-  const [findPost] = posts.filter(post => post.title === params)
-  let msg
+  const [findPost] = posts.filter(post => {
+    const title = _.kebabCase(post.title);
+    const titleParams = _.kebabCase(params);
+    if (title === titleParams) {
+      return true
+    }
+  })
+  
   if (findPost) {
-    msg = 'Found'
+    res.render('post', { title: findPost.title, content: findPost.content })
   } else {
-    msg = 'Not found'
+    res.render('post', {title: 'Error', content: 'Nothing Has found'})
   }
-  console.log(msg);
-  res.send('welcome ' + params)
 })
 
 app.post('/compose', (req, res) => {
