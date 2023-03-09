770afb48d9427cf5a9de0033966cf08ea9127bb9
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-10 00:16:56 +0100

    Add multiple Fruits using insertMany

--- mongoose/index.js
+++ mongoose/index.js
@@ -15,6 +15,20 @@ const appel = new Fruit({
     rating: 7,
     review: 'Umm Im Ok'
 });
+
+
+const banana = new Fruit({
+    name: 'Banana',
+    rating: 10,
+    review: 'Im the best Hehe'
+});
+
+const kiwi = new Fruit({
+    name: 'Kiwi',
+    rating: 9,
+    review: 'Trying to be the best'
+});
+Fruit.insertMany([banana, kiwi]);
 // appel.save();
 
 
@@ -29,4 +43,32 @@ const person = new Person({
     age: 36
 });
 
-person.save();
+// person.save();
+
+const countrySchema = new mongoose.Schema({
+    name: String,
+    capital: String,
+    population: Number
+});
+
+const Country = mongoose.model('Country', countrySchema);
+const country = new Country({
+    name: 'Morocco',
+    capital: 'Rabat',
+    population: 36910558 
+});
+const country2 = new Country({
+    name: 'Morocco',
+    capital: 'Rabat',
+    population: '36910558' 
+});
+const country3 = new Country({
+    name: 'Canada',
+    capital: 'Ottawa',
+    population: 38005238 
+});
+
+
+// country3.save();
+// country2.save()
+// country.save();
