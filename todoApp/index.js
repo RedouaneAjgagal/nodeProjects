651cfa2648694d03763566db798ebe366d0e4ab2
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-10 20:44:07 +0100

    Display default todos from todosListDB

--- todoApp/index.js
+++ todoApp/index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const app = express();
 const generate = require(`${__dirname}/generateId.js`)
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todoListDB');
+
+const todoSchema = new mongoose.Schema({
+    value: {
+        type: String,
+        required: [true, 'Cannot be empty']
+    },
+    completed: Boolean
+});
+const Todo = mongoose.model('Todo', todoSchema);
+
+
+
 let todos = []
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index', { todos })
+    const getTodos = async () => {
+        const todos = await Todo.find()
+        if (!todos.length) {
+            const firstTodo = new Todo({ value: '10 min walk', completed: false })
+            const secondTodo = new Todo({ value: 'study mongoose', completed: false })
+            const thirdTodo = new Todo({ value: 'watch an episode of any anime', completed: false })
+            const initialList = [firstTodo, secondTodo, thirdTodo]
+            Todo.insertMany(initialList);
+            res.redirect('/')
+        } else {
+            res.render('index', { todos })
+        }
+    }
+    getTodos()
 })
 
 app.post('/', (req, res) => {
