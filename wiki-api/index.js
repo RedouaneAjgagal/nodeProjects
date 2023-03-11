c4b0aa877de63f345096687b2faf7c67afe236f8
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-11 23:05:41 +0100

    Setup wiki-api

---
+++ wiki-api/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+
+const articlesSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+const Article = mongoose.model('Article', articlesSchema);
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port: 3000`);
+})
+
+
+
+
+
+
+
+
+
+
+
+// const insertMany = async () => {
+//     const article1 = new Article({
+//         title: "The Benefits of Meditation for Mental Health",
+//         content: "Meditation has been shown to reduce symptoms of anxiety, depression, and stress."
+//     })
+//     const article2 = new Article({
+//         title: "The Importance of Sleep for Overall Health",
+//         content: "Getting enough sleep is crucial for maintaining a healthy immune system, proper cognitive function, and overall physical health."
+//     })
+//     const article3 = new Article({
+//         title: "The Effects of Social Media on Mental Health",
+//         content: " Studies have found a correlation between social media use and increased feelings of anxiety, depression, and loneliness, especially among younger generations."
+//     })
+//     const allArticles = [article1, article2, article3]
+//     await Article.insertMany(allArticles)
+// }
+// insertMany()
