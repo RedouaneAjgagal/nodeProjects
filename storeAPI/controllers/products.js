9f1bc8b779d8b72f8bd3723cbf6ec0d435b43b5c
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-17 02:52:16 +0100

    Add filtering & sorting

--- storeAPI/controllers/products.js
+++ storeAPI/controllers/products.js
@@ -1,6 +1,23 @@
+const Product = require('../models/product');
+
+// Read all products
 const getProducts = async (req, res) => {
-    throw new Error('testing async errors handler package!');
-    return res.status(200).json({ msg: 'All products' });
+    const { name, featured, company, sort } = req.query
+    const queryObj = {}
+    if (name) {
+        queryObj.name = { $regex: name, $options: 'i' }
+    }
+    if (featured) {
+        queryObj.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObj.company = company
+    }
+    // console.log(sort.split(',').join(' '));
+    const sortList = sort ? sort.split(',').join(' ') : 'createdAt';
+    const products = await Product.find(queryObj).setOptions({ strictQuery: true }).sort(sortList);
+
+    return res.status(200).json(products);
 }
 
 
