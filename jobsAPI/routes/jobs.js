92173e8dbd60663119b6163d80349005986f3f52
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-18 22:31:29 +0100

    Setup the project

---
+++ jobsAPI/routes/jobs.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {getAllJobs, getJob, createJob, updateJob, deleteJob} = require('../controllers/jobs')
+
+
+router.route('/jobs')
+    .get(getAllJobs)
+    .post(createJob);
+
+
+router.route('/jobs/:id')
+    .get(getJob)
+    .patch(updateJob)
+    .delete(deleteJob);
+
+
+module.exports = router
