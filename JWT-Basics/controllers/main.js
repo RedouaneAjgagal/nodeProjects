3c71eaad0bc7b2ce51d2f1f26dcee78f06a6571b
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-18 01:25:15 +0100

    Add token on login

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -1,10 +1,21 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+
+
 const login = async (req, res) => {
-    res.status(200).send(`Welcome back user`)
+    const { username, password } = req.body
+    if (username.trim().length < 1 || password.trim().length < 1) {
+        throw new CustomAPIError('Username or Password cannot be empty.', 400)
+    }
+    // demo ID
+    const id = Math.random();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    res.status(200).json({ msg: `Welcome back ${username}`, token })
 }
 
 const dashboard = async (req, res) => {
     const authenticatedData = Math.floor((Math.random() * 100) + 1);
-    res.status(200).send(`Your private key is ${authenticatedData}`);
+    res.status(200).json({ msg: `Hello, Max`, secret: `Your private key is ${authenticatedData}` });
 }
 
 
--- JWT-Basics/routes/main.js
+++ JWT-Basics/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { login, dashboard } = require('../controllers/main')
 
-router.get('/login', login);
-router.post('/dashboard', dashboard);
+router.post('/login', login);
+router.get('/dashboard', dashboard);
 
 module.exports = router;
