3eb71917ae27dd43647a3cf141d7c008d82a02d7
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-20 18:56:42 +0100

    Add more security

--- jobsAPI/app.js
+++ jobsAPI/app.js
@@ -7,12 +7,28 @@ const jobsRouter = require('./routes/jobs');
 const authRouter = require('./routes/auth');
 const authUser = require('./middleware/authentication');
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xssCleaner = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xssCleaner());
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
