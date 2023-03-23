060108516843504d9b0403a9aa5b48ac59df7322
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-23 05:18:56 +0100

    Add upload validation

--- file-upload-app/controllers/uploadsController.js
+++ file-upload-app/controllers/uploadsController.js
@@ -4,12 +4,20 @@ const { BadRequestError } = require('../errors')
 const path = require('path');
 
 const uploadProductImage = async (req, res) => {
+    if (!req.files) {
+        throw new BadRequestError('Image is required!')
+    }
     const uploadImage = req.files.image;
     const imageType = uploadImage.mimetype.split('/')[0]
     if (imageType !== 'image') {
         throw new BadRequestError('Only images are valid')
     }
-    const imagePath = path.join(__dirname, '../public/uploads', uploadImage.name);
+
+    const maxSize = 1024 * 1024
+    if (uploadImage.size > maxSize) {
+        throw new BadRequestError('Please upload image smaller than 1KB');
+    }
+        const imagePath = path.join(__dirname, '../public/uploads', uploadImage.name);
     await uploadImage.mv(imagePath);
     res.status(StatusCodes.OK).json({ image: { src: `uploads/${uploadImage.name}` } })
 }
