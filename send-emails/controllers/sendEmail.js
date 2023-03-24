5b85be15a99225187f34f9dbcc503502d71c006b
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-24 05:22:08 +0100

    Send test mails using sendmailer & Ethereal's fake accs

--- send-emails/controllers/sendEmail.js
+++ send-emails/controllers/sendEmail.js
@@ -1,5 +1,24 @@
+const nodemailer = require('nodemailer');
+
+
 const sendEmail = async (req, res) => {
-    res.send('Send email func')
+    await nodemailer.createTestAccount();
+    const transport = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'kane.bernhard46@ethereal.email',
+            pass: 'grBWJ3XaQTrRGqG7Ac'
+        }
+    });
+    const info = await transport.sendMail({
+        from: 'Sender Name <sender@example.com>',
+        to: 'Recipient <recipient@example.com>',
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    })
+    res.json(info)
 }
 
 module.exports = sendEmail;
