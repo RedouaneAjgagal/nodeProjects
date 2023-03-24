9808f288abf8e93eec4723dccbbfa56ec71f0941
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-24 16:08:21 +0100

    Send emails using sendinblue with and without nodemailer

--- send-emails/controllers/sendEmail.js
+++ send-emails/controllers/sendEmail.js
@@ -1,7 +1,10 @@
 const nodemailer = require('nodemailer');
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 
-const sendEmail = async (req, res) => {
+// Send test emails using nodemailer and ethereal
+const sendTestEmail = async (req, res) => {
     await nodemailer.createTestAccount();
     const transport = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
@@ -19,6 +22,51 @@ const sendEmail = async (req, res) => {
         html: '<p><b>Hello</b> to myself!</p>'
     })
     res.json(info)
+}
+
+// send emails using nodemailer and Sendinblue
+const sendEmails = async (req, res) => {
+    const transport = nodemailer.createTransport({
+        host: 'smtp-relay.sendinblue.com',
+        port: 587,
+        auth: {
+            user: 'test@mail.co',
+            pass: process.env.SIB_PASSWORD
+        }
+    });
+    const info = await transport.sendMail({
+        from: 'Sender Name <test@mail.co>',
+        to: 'Recipient <tomail@mail.co>',
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    })
+    res.json(info)
+}
+
+// send emails using only Sendinblue
+const sendEmail = async (req, res) => {
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.SIB_API_KEY;
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    sendSmtpEmail = {
+        to: [{
+            email: 'test@mail.co',
+            name: 'Jhon Doe'
+        }],
+        templateId: 4,
+        params: {
+            name: 'Jhon',
+            surname: 'Doe'
+        },
+        headers: {
+            'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
+        }
+    };
+    const info = await apiInstance.sendTransacEmail(sendSmtpEmail)
+    res.json(info)
+
 }
 
 module.exports = sendEmail;
